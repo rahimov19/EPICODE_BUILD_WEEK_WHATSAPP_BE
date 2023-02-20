43dca4ececef187ce99abc568cd232ded602e773
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-02-20 12:42:08 +0000

    fixes user endpoints

--- src/apis/users/index.js
+++ src/apis/users/index.js
@@ -6,6 +6,8 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { v2 as cloudinary } from "cloudinary";
 import q2m from "query-to-mongo";
 import passport from "passport";
+import { JWTAuthMiddleware } from "../../library/authentication/jwtAuth.js";
+import { createAccessToken } from "../../library/authentication/jwtTools.js";
 
 const cloudinaryUploader = multer({
   storage: new CloudinaryStorage({
@@ -30,78 +32,101 @@ usersRouter.get("/", async (req, res, next) => {
       mongoQuery.criteria,
       mongoQuery.options.fields
     );
-
     res.send(users);
-  } catch {
+  } catch (error) {
     next(error);
   }
 });
 
 usersRouter.post("/register", async (req, res, next) => {
   try {
+    // const newUser = new UsersModel(req.body);
+    // const { _id } = await newUser.save();
+    // res.status(201).send({ _id });
+
+    const { email, password, username } = req.body;
+    const emailAlreadyRegistered = await UsersModel.findOne({ email: email });
+    if (emailAlreadyRegistered) {
+      return next(
+        createHttpError(400, `User with provided email already exists`)
+      );
+    }
     const newUser = new UsersModel(req.body);
-    const { _id } = await newUser.save();
-    res.status(201).send({ _id });
+    await newUser.save();
+    if (
+      (newUser && email && password && username) ||
+      (newUser && email && password && username && avatar)
+    ) {
+      const payload = { _id: newUser._id, role: newUser.role };
+
+      const accessToken = await createAccessToken(payload);
+      res.status(201).send({ user: newUser, accessToken: accessToken });
+    }
   } catch (error) {
     next(error);
   }
 });
 
-// usersRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
-//   try {
-//     const user = await UsersModel.findById(req.user._id);
-//     res.send(user);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+usersRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.user._id);
+    if (user) {
+      res.send(user);
+    } else {
+      next(createHttpError(404, `User with provided id not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-// usersRouter.put("/me", JWTAuthMiddleware, async (req, res, next) => {
-//   try {
-//     const user = await UsersModel.findById(req.user._id);
-//     if (user) {
-//       //the req.user is updated by the JWTAuthMiddleware
-//       //we use the verifyAccessToken function which resolves the promise
-//       //(and returns the payload: with _id and the role)
-//       //and if it rejects the promise, it return an error
+usersRouter.put("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.user._id);
+    if (user) {
+      //the req.user is updated by the JWTAuthMiddleware
+      //we use the verifyAccessToken function which resolves the promise
+      //(and returns the payload: with _id and the role)
+      //and if it rejects the promise, it return an error
 
-//       const updatedUser = await UsersModel.findByIdAndUpdate(
-//         req.user._id,
-//         req.body,
-//         { new: true, runValidators: true }
-//       );
-//       res.status(204).send(updatedUser);
-//     } else {
-//       next(createHttpError(404, `User with the provided id not found`));
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+      const updatedUser = await UsersModel.findByIdAndUpdate(
+        req.user._id,
+        req.body,
+        { new: true, runValidators: true }
+      );
+      res.status(204).send(updatedUser);
+    } else {
+      next(createHttpError(404, `User with the provided id not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-// usersRouter.post(
-//   "/me/avatar",
-//   JWTAuthMiddleware,
-//   cloudinaryUploader,
-//   async (req, res, next) => {
-//     try {
-//       //we get from req.body the picture we want to upload
-//       console.log(req.file.mimetype);
-//       const updatedUser = await UsersModel.findByIdAndUpdate(
-//         req.user._id,
-//         { image: req.file.path },
-//         { new: true, runValidators: true }
-//       );
-//       if (updatedUser) {
-//         res.status(204).send(updatedUser);
-//       } else {
-//         next(createHttpError(404, `User with id ${req.user._id} not found`));
-//       }
-//     } catch (error) {
-//       next(error);
-//     }
-//   }
-// );
+usersRouter.post(
+  "/me/avatar",
+  JWTAuthMiddleware,
+  cloudinaryUploader,
+  async (req, res, next) => {
+    try {
+      //we get from req.body the picture we want to upload
+      console.log(req.file.mimetype);
+      console.log("ID: ", req.user._id);
+      const updatedUser = await UsersModel.findByIdAndUpdate(
+        req.user._id,
+        { avatar: req.file.path },
+        { new: true, runValidators: true }
+      );
+      if (updatedUser) {
+        res.status(204).send(updatedUser);
+      } else {
+        next(createHttpError(404, `User with id ${req.user._id} not found`));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 usersRouter.get("/:userId", async (req, res, next) => {
   try {
@@ -111,7 +136,7 @@ usersRouter.get("/:userId", async (req, res, next) => {
     } else {
       next(createHttpError(404, `User with provided id not found`));
     }
-  } catch {
+  } catch (error) {
     next(error);
   }
 });
